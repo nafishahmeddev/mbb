4c752d1a9d50e005b0df80590c4be5975aa6c58d
Nafish Ahmed <nafish.ahmed.dev@gmail.com>
2020-07-04 08:28:08 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+let app = require('express')();
+let http = require('http').createServer(app);
+let io = require('socket.io')(http);
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/app.html');
+});
+
+io.on('connection', (socket) => {
+    socket.on('start', (msg) => {
+        io.emit('event', msg);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+});
+
+
+http.listen(80, () => {
+    console.log('listening on *:3000');
+});
